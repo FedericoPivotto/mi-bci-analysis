3e2088bb85dfce6983883e330a8baafc99225bd4
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2025-01-05 23:20:32 +0100

    Fix naming

--- solution/function/compute_featuremap.m
+++ solution/function/compute_featuremap.m
@@ -15,13 +15,13 @@ function compute_featuremap(dirpath_in, filename, fileext, dirpath_out)
     [psd_mat.LABEL.Tk, psd_mat.LABEL.Ck, psd_mat.LABEL.CFbK, psd_mat.LABEL.Pk, psd_mat.LABEL.Mk] = get_label_vectors(psd_mat.PSD, psd_mat.EVENT, 'offline');
     
     % 3. Compute Fisher's score
-    fisher_scores = get_fisher_score(psd_mat.PSD, psd_mat.LABEL.Pk);
+    fisher_scores = get_fisher_scores(psd_mat.PSD, psd_mat.LABEL.Pk);
     
     % 4. Save feature's map
-    featuremap_filename = fullfile(dirpath_out, [filename, '_featuremap', fileext]);
-    save(featuremap_filename, 'fisher_scores');
+    % featuremap_filename = fullfile(dirpath_out, [filename, '_featuremap', fileext]);
+    % save(featuremap_filename, 'fisher_scores');
 
-     % 5. Visualize the feature map
+    % 5. Visualize the feature map
     n_channels = size(psd_mat.PSD, 3); % Number of channels
     channel_labels = {'Fz', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'C3', 'C1', 'Cz', 'C2', 'C4', 'CP3', 'CP1', 'CPz', 'CP2', 'CP4'};
     clim = [0 1]; % Color limits for visualization
@@ -39,4 +39,6 @@ function compute_featuremap(dirpath_in, filename, fileext, dirpath_out)
         'XTick', FREQ_subset, ...
         'YTickLabel', keys(channels), ...
         'YTick', values(channels));
+
+    % TODO: save feature map plot
 end
