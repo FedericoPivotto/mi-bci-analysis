f11ca727993a79506884cf0f7d1907549555dc12
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2025-01-10 11:19:38 +0100

    Necessary info

--- solution/evaluation.m
+++ solution/evaluation.m
@@ -4,5 +4,12 @@ clc; close all; clear;
 %% Function path
 addpath('function/');
 
+% function evaluate_model(path_model, dirpath_in, filename, fileext, dirpath_out)
+% INFO: path_model: 'solution/model/micontinuous/<subject>/<filename_with_ext>', 'solution/model/micontinuous/population/<filename_with_ext>'
+% INFO: dirpath_in: 'solution/psd/micontinuous/<subject>/', 'solution/psd/micontinuous/population/'
+% INFO: filename: '<filename_without_ext>'
+% INFO: fileext: '.mat'
+% INFO: dirpath_out: 'solution/model/micontinuous/<subject>/', 'solution/model/micontinuous/population/'
+
 %% Evaluate subjects and population trained classification models
 
--- solution/training.m
+++ solution/training.m
@@ -4,5 +4,11 @@ clc; close all; clear;
 %% Function path
 addpath('function/');
 
+% function train_model(dirpath_in, filename, fileext, dirpath_out)
+% INFO: dirpath_in: 'solution/psd/micontinuous/<subject>/', 'solution/psd/micontinuous/population/'
+% INFO: filename: '<filename_without_ext>'
+% INFO: fileext: '.mat'
+% INFO: dirpath_out: 'solution/model/micontinuous/<subject>/', 'solution/model/micontinuous/population/'
+
 %% Train subjects and population classification models
 
