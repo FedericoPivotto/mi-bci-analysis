9a5f9b2849f0befdce6e4c70d549e403df922643
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2025-01-10 11:13:59 +0100

    Base structure

--- solution/evaluation.m
+++ solution/evaluation.m
@@ -1,3 +1,8 @@
 %% Workspace setup
 clc; close all; clear;
 
+%% Function path
+addpath('function/');
+
+%% Evaluate subjects and population trained classification models
+
--- solution/training.m
+++ solution/training.m
@@ -1,3 +1,8 @@
 %% Workspace setup
 clc; close all; clear;
 
+%% Function path
+addpath('function/');
+
+%% Train subjects and population classification models
+
