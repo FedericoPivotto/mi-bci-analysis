6cc79ce2bec2809f1af8f551ae718467fd842809
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2025-01-14 18:34:20 +0100

    Fix plot for offline

--- solution/analysis.m
+++ solution/analysis.m
@@ -64,7 +64,16 @@ for i = 1:size(subject, 2)
 
     % Compute spectrograms and feature maps for retrieved psd files
     for h = 1:size(filename_psd,2)
-        compute_spectrogram(dirpath_subject_psd, filename_psd(1,h), '.mat', dirpath_spectrograms_subject);
-        compute_featuremap(dirpath_subject_psd, filename_psd(1,h), '.mat', dirpath_featuremaps_subject);
+        % Consider concatenated recordings
+        if (contains(filename_psd(1,h), strcat(subject_id, '.online'))) || (contains(filename_psd(1,h), strcat(subject_id, '.offline')))
+            compute_spectrogram(dirpath_subject_psd, filename_psd(1,h), '.mat', dirpath_spectrograms_subject);
+            compute_featuremap(dirpath_subject_psd, filename_psd(1,h), '.mat', dirpath_featuremaps_subject);
+            continue;
+        end
+        if contains(filename_psd(1,h), 'offline')
+            compute_spectrogram(dirpath_subject_psd, filename_psd(1,h), '.mat', dirpath_spectrograms_subject);
+            compute_featuremap(dirpath_subject_psd, filename_psd(1,h), '.mat', dirpath_featuremaps_subject);
+            continue;
+        end
     end
 end
